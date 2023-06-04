9059659a006c38c61c7230650d61bef85604d5c8
Jason Lin <smallhanley@gmail.com>
2023-06-04 23:33:03 +0800

    Add timing

--- test/visual_sample.py
+++ test/visual_sample.py
@@ -4,16 +4,16 @@ from ggdGVC import graph
 
 # Contruct graph
 G = graph()
-G.add_edge(0, 1)
-G.add_edge(0, 2)
-G.add_edge(1, 2)
-G.add_edge(1, 4)
-G.add_edge(2, 4)
-G.add_edge(3, 4)
-G.add_vertex(17)
+# G.add_edge(0, 1)
+# G.add_edge(0, 2)
+# G.add_edge(1, 2)
+# G.add_edge(1, 4)
+# G.add_edge(2, 4)
+# G.add_edge(3, 4)
+G.read_graph('complete_graph.json')
 
 # Set coloring method
-G.set_method(graph.GREEDY)
+G.set_method(graph.BACKTRACKING)
 
 # Color graph
 G.color_graph()
@@ -22,14 +22,16 @@ G.color_graph()
 vertex = G.load_vertex()
 edge = G.load_edge()
 color = G.load_color()
-G.write_graph('tmp.json')
+num = G.get_color_num()
+print(color)
+print(num)
 
 # Visualization
-G = nx.Graph()
-G.add_nodes_from(vertex)
-G.add_edges_from(edge)
-color_map = []
-for node in G:
-    color_map.append(color[node])
-nx.draw(G, node_color=color_map, with_labels=True)
-plt.show()
+# G = nx.Graph()
+# G.add_nodes_from(vertex)
+# G.add_edges_from(edge)
+# color_map = []
+# for node in G:
+#     color_map.append(color[node])
+# nx.draw(G, node_color=color_map, with_labels=True)
+# plt.show()
